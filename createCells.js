eb2f617beba63238d7627e6517fc4db62720ee64
raymond <raymondezra276@gmail.com>
2023-07-14 13:07:26 +0300

    updates(comm)

--- createCells.js
+++ createCells.js
@@ -1,6 +1,6 @@
 function createCells(req, res, next) {
     const cellsArray = [];
-  
+//This 'for loop' used to add objects to the cells array 
     for (let i = 0; i < 9; i++) {
       cellsArray.push({
         value: "",
@@ -8,10 +8,10 @@ function createCells(req, res, next) {
         clicked: false,
       });
     }
-  
+//Next functio is called to pass control to the next middleware 
     return cellsArray
     next();
   }
-  
+//exports the create cell so to be used in other functions
   module.exports = createCells
   
--- db.js
+++ db.js
@@ -1,10 +1,15 @@
+//used for  importing the Mongoose library, 
 const mongoose = require("mongoose")
-
+//Whole function used  to connect to MongoDB
 const connectDB = async () => {
   try {
+// Ensures to wait for the connection to be established    
     await mongoose.connect("mongodb+srv://Evarest:Evarest3@cluster0.7oqnlw3.mongodb.net/TicTacToe?retryWrites=true&w=majority");
+//If the connection is successful message is returned    
     console.log("MongoDB connected");
-  } catch (err) {
+  } 
+//If the connection is unsuccessful message is returned  
+  catch (err) {
     console.error("Error connecting to MongoDB:", err);
   }
 };
